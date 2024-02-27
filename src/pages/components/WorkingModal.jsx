f2a878f1979cb264913c7839dd9c40675ba5de23
MIke Shields <codingshields@gmail.com>
2024-02-27 15:57:24 -0700

    test

--- src/pages/components/WorkingModal.jsx
+++ src/pages/components/WorkingModal.jsx
@@ -6,7 +6,7 @@ const WorkingModal = () => {
 		<div className='absolute bg-opacity-50 bg-gray-500 w-full h-full z-10 top-0 left-0'>
 			<div className='flex flex-row justify-center items-center place-content-center w-full h-full'>
 					<h1 className='w-full text-2xl font-bold animate-pulse text-white text-center '>
-						Uploading... <img src={workingGears} className='w-32 h-32 mx-auto' />
+						Uploading...test <img src={workingGears} className='w-32 h-32 mx-auto' />
 					</h1>
 			</div>
 		</div>
