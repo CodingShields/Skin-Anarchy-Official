ed22ee23cea5a706b51661c9c2d9ab984570e24f
Mike Shields <139907470+CodingShields@users.noreply.github.com>
2024-03-26 14:31:08 -0600

    Update and rename homeLayout.jsx to HomeLayout.jsx

--- src/pages/layout/HomeLayout.jsx
+++ src/pages/layout/HomeLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const HomeLayout = () => {
 	const location = useLocation();
