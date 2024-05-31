ba1da5aa16305ca959e0dbd5417aae8fcb74345d
CodingShields <CodingShields@gmail.com>
2024-05-31 14:14:55 -0600

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Ensure process is defined
-if (typeof process === "undefined") {
-	globalThis.process = { env: {} };
-}
-
 export default defineConfig(({ mode }) => {
 	// Load env file based on `mode` in the current working directory.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
@@ -13,13 +8,9 @@ export default defineConfig(({ mode }) => {
 
 	return {
 		plugins: [react()],
-		// Add assetsInclude option to include HTML files as assets
-		assetsInclude: ["**/*.html"],
 		// vite config
 		define: {
-			"process.env": {
-				VITE_FIREBASE_API_KEY: JSON.stringify(env.VITE_FIREBASE_API_KEY), // Make the env var available to your React client-side code
-			},
+			VITE_API_KEY: JSON.stringify(env.VITE_FIREBASE_API_KEY), // Make the env var available to your React client-side code
 		},
 	};
 });
