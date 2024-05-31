5f74412e9b03a4f30dd54c2e15798390ebf9c6ee
CodingShields <CodingShields@gmail.com>
2024-05-31 14:17:45 -0600

    test

--- src/pages/blog/SkinAnarchyBlog.jsx
+++ src/pages/blog/SkinAnarchyBlog.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-import test from "../../assets/data/mediumData/test.html";
 import "../../styles/custom.scss";
-import blogBG from "../../assets/video/blogBG.mp4";
 import PrevBlogs from "./PrevBlogs";
 import ActiveBlogWindow from "./ActiveBlogWindow";
 import BlogUserResponse from "./BlogUserResponse";
@@ -10,74 +8,74 @@ const SkinAnarchyBlog = () => {
 	const [title, setTitle] = useState("");
 	const [activeBlogWindow, setActiveBlogWindow] = useState("");
 	const [openBlogWindow, setOpenBlogWindow] = useState(false);
-	useEffect(() => {
-		const iframe = document.getElementById("mediumImport");
+	// useEffect(() => {
+	// 	const iframe = document.getElementById("mediumImport");
 
-		const onLoad = () => {
-			const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
-			const root = iframeDocument.querySelector(".h-entry");
-			const styleElement = iframeDocument.createElement("style");
+	// 	const onLoad = () => {
+	// 		const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+	// 		const root = iframeDocument.querySelector(".h-entry");
+	// 		const styleElement = iframeDocument.createElement("style");
 
-			// Set the content of the <style> element with the @import rule
-			styleElement.textContent = `
-            @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+	// 		// Set the content of the <style> element with the @import rule
+	// 		styleElement.textContent = `
+    //         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
 
-            /* Override the font family for all elements */
-            * {
-                font-family: 'Montserrat', sans-serif !important;
-            }
-        `;
-			const title = iframeDocument.querySelector(".p-name");
-			setTitle(title.innerText);
-			console.log();
-			const body = iframeDocument.body;
-			if (body) {
-				body.style.overflowY = "hidden";
-				body.style.overflowX = "hidden";
-			}
+    //         /* Override the font family for all elements */
+    //         * {
+    //             font-family: 'Montserrat', sans-serif !important;
+    //         }
+    //     `;
+	// 		const title = iframeDocument.querySelector(".p-name");
+	// 		setTitle(title.innerText);
+	// 		console.log();
+	// 		const body = iframeDocument.body;
+	// 		if (body) {
+	// 			body.style.overflowY = "hidden";
+	// 			body.style.overflowX = "hidden";
+	// 		}
 
-			// Append the <style> element to the <head> of the iframe document
-			const head = iframeDocument.head || iframeDocument.getElementsByTagName("head")[0];
-			head.appendChild(styleElement);
+	// 		// Append the <style> element to the <head> of the iframe document
+	// 		const head = iframeDocument.head || iframeDocument.getElementsByTagName("head")[0];
+	// 		head.appendChild(styleElement);
 
-			if (root) {
-				// Example: Change font family for all elements
-				root.style.fontFamily = "Montserrat";
+	// 		if (root) {
+	// 			// Example: Change font family for all elements
+	// 			root.style.fontFamily = "Montserrat";
 
-				// Example: Change styles for h1, h2, h3 elements
-				const headings = root.querySelectorAll("h1, h2, h3");
-				headings.forEach((heading) => {
-					heading.style.color = "black";
-					heading.style.fontFamily = "Montserrat";
-					heading.style.fontWeight = 500;
-				});
+	// 			// Example: Change styles for h1, h2, h3 elements
+	// 			const headings = root.querySelectorAll("h1, h2, h3");
+	// 			headings.forEach((heading) => {
+	// 				heading.style.color = "black";
+	// 				heading.style.fontFamily = "Montserrat";
+	// 				heading.style.fontWeight = 500;
+	// 			});
 
-				// Example: Remove the footer
-				const footer = root.querySelector("footer");
-				if (footer) {
-					footer.remove();
-				}
-			}
-		};
+	// 			// Example: Remove the footer
+	// 			const footer = root.querySelector("footer");
+	// 			if (footer) {
+	// 				footer.remove();
+	// 			}
+	// 		}
+	// 	};
 
-		if (iframe) {
-			iframe.addEventListener("load", onLoad);
-		}
+	// 	if (iframe) {
+	// 		iframe.addEventListener("load", onLoad);
+	// 	}
 
-		return () => {
-			if (iframe) {
-				iframe.removeEventListener("load", onLoad);
-			}
-		};
-	}, []);
+	// 	return () => {
+	// 		if (iframe) {
+	// 			iframe.removeEventListener("load", onLoad);
+	// 		}
+	// 	};
+	// }, []);
 
-	useEffect(() => {
-		if (activeBlogWindow) {
-			setOpenBlogWindow(!openBlogWindow);
-		} else {
-			setOpenBlogWindow(false);
-		}
-	}, [activeBlogWindow]);
+	// useEffect(() => {
+	// 	if (activeBlogWindow) {
+	// 		setOpenBlogWindow(!openBlogWindow);
+	// 	} else {
+	// 		setOpenBlogWindow(false);
+	// 	}
+	// }, [activeBlogWindow]);
 
 	const handleOnSelect = (e) => {
 		console.log("test");
@@ -121,7 +119,7 @@ const SkinAnarchyBlog = () => {
 					</div>
 				</div>
 			</div>
-			<ActiveBlogWindow open={openBlogWindow} close={() => setOpenBlogWindow(false)} data={activeBlogWindow} />
+			{/* <ActiveBlogWindow open={openBlogWindow} close={() => setOpenBlogWindow(false)} data={activeBlogWindow} /> */}
 			<div className={openBlogWindow ? "grid grid-cols-3 blur-sm " : "flex flex-row justify-between"}>
 				<div className=' pt-4 w-full h-screen  font-montserrat bg-white text-2xl text-center overflow-y-scroll space-y-8'>
 					<h1 onSelect={handleOnSelect} className='text-5xl font-montserrat py-4'>
