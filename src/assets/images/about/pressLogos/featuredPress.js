4f658c7702e52434deaca2765e553b1f4ce658e5
MIke Shields <codingshields@gmail.com>
2024-06-15 17:10:25 -0600

    featuted press

--- src/assets/images/about/pressLogos/featuredPress.js
+++ src/assets/images/about/pressLogos/featuredPress.js
@@ -1,3 +1,4 @@
+import { name } from "@cloudinary/url-gen/actions/namedTransformation";
 import image10 from "./image10.svg";
 import image11 from "./image11.svg";
 import image12 from "./image12.svg";
@@ -26,4 +27,129 @@ import image34 from "./image34.svg";
 import image35 from "./image35.svg";
 
 
-export  const imageArray = [ image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27, image28, image29, image30, image31, image32, image33, image34, image35 ];
+export const imageArray = [
+    {
+    name: "forbes new york",
+    link:"https://forbesnewyork.com/skincare-anarchy-launches-s-a-f-e-seal-a-revolutionary-breakthrough-in-scientific-validation-of-beauty-and-wellness-products/",
+    image: image10
+    },
+    {
+    name: "fox 8 news",
+    link:"https://fox8.com/business/press-releases/ein-presswire/624527931/meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions/",
+    image: image11,
+    },
+    {
+        name: "fox 40 news",
+        image: image12,
+        link: "https://fox40.com/business/press-releases/ein-presswire/624527931/meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions/",
+    },
+    {
+        name: "market watch",
+        image:image13,
+        link: "https://www.marketwatch.com/press-release/skincare-anarchy-launching-yugen-magazine-volume-2-2023-06-05?mod=search_headline"
+    },
+    {
+        name: "new beauty",
+        image: image14,
+        link: "https://www.newbeauty.com/skincare-anarchy-interview/"
+    },
+    {
+        name: "entreprenuer",
+        image: image15,
+        link: "no link",
+    }, {
+        name: "forbes morocco",
+        image: image16,
+        link: "https://forbesmorocco.com/2023/06/07/dr-ekta-yadav-pioneering-a-new-wave-of-medical-communication-through-skincare-anarchy-the-podcast-and-yugen-magazine/",
+    },
+    {
+        name: "beauty independent",
+        image: image17,
+        link: "https://www.beautyindependent.com/what-science-washing-can-the-beauty-industry-d",
+    },
+    {
+        name: "yahoo",
+        image: image18,
+        link: "https://finance-yahoo-com.cdn.ampproject.org/c/s/finance.yahoo.com/amphtml/news/skincare-anarchy-launching-y-gen-150000698.html"
+    },
+    {
+        name: "the asia gazette",
+        image: image20,
+        link: "https://www.theasiagazette.com/article/624527931-meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions",
+    },
+    {
+        name: "cew",
+        image: image21,
+        link: "https://cew.org/beauty_news/skincare-anarchy-the-skin-authority-podcast-you-need-to-know/"
+    },
+    {
+        name: "japan business post",
+        image: image22,
+        link: "https://www.japanbusinesspost.com/article/624527931-meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions  "
+    },
+    {
+        name: "the pr net",
+        image: image23,
+        link: "https://theprnet.com/journals/podcast-spotlight-skincare-anarchy"
+    },
+    {
+        name: "pix11",
+        image: image24,
+        link: "https://pix11.com/business/press-releases/ein-presswire/624527931/meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions/"
+    },
+    {
+        name: "ap news",
+        image: image25,
+        link: "https://apnews.com/press-release/ein-presswire-newsmatics/health-ein-presswire-newsmatics-e8a4c817f115758df4afec08372a5b73"
+    },
+    {
+        name: "the garnetter report",
+        image: image26,
+        link: " https://thegarnettereport.com/beauty/new-podcast-skincare-anarchy/"
+    },
+    {
+        name: "canvas rebel",
+        image: image27,
+        link: "https://canvasrebel.com/meet-ekta-yadav/",
+    },
+    {
+        name: " marie reynolds london",
+        image: image28,
+        link: "https://www.mariereynoldslondon.com/pages/good-vibes-only-podcast"
+    },
+    {
+        name: "ein press wire",
+        image: image29,
+        link: "no link"
+    },
+    {
+        name: "pop sugar",
+        image: image30,
+        link: "https://www.popsugar.com/beauty/best-beauty-podcasts-48112081#photo-48281634"
+    },
+    {
+        name: "business insider",
+        image: image31,
+        link: "no link"
+    },
+    {
+        name: "global cosmetic indesutry",
+        image: image32,
+        link: "https://www.gcimagazine.com/brands-products/news/news/21867019/podcast-alert-skincare-anarchy"
+    },
+    {
+        name: "news 10 abc",
+        image: image33,
+        link: "https://www.news10.com/business/press-releases/ein-presswire/624527931/meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions/"
+    },
+    {
+        name: "beauty korean news",
+        image: image34,
+        link: "http://www.bkn24.com/news/articleView.html?idxno=14151"
+    },
+    {
+        name: "business daily switzerland",
+        image: image35,
+        link: "https://www.switzerlandweekly.com/article/624527931-meet-the-surgeon-the-founder-dr-ekta-yadav-is-impacting-the-world-by-sharing-stories-innovations-and-visions"
+    },
+    ];
--- src/pages/about/FeaturedPressComp.jsx
+++ src/pages/about/FeaturedPressComp.jsx
@@ -1,12 +1,36 @@
-import {imageArray} from "../../assets/images/about/pressLogos/featuredPress"
-
+import {useState, useEffect} from "react";
+import { imageArray } from "../../assets/images/about/pressLogos/featuredPress"
+import FeaturedPressModal from "../components/FeaturedPressModal"
 const FeaturedPress = () => {
+	const [state, setState] = useState({
+		loading: false,
+		error: false,
+		message: "",
+	})
+
+	const [featuredPressLink, setFeaturedPressLink] = useState({
+		link: "",
+		openModal: false,
+	})
+	const handleMouseOver = (e) => {
+		setState({
+			...state,
+			error: false,
+			message: "",
+		})
+		setFeaturedPressLink({link: e.target.name, openModal: true})
+	}
+
 	return (
 		<div className='min-h-screen  w-full flex flex-col items-center justify-center bg-black mt-36'>
+			<FeaturedPressModal link={featuredPressLink.link} open={featuredPressLink.openModal} close={() => setFeaturedPressLink({link: "", openModal: false})} />
 			<h1 className="text-white text-3xl font-montserrat font-thin tracking-widest underline underline-offset-4 decoration-1 uppercase">Featured Press Page</h1>
+			<p className="text-white text-xl font-montserrat font-thin tracking-widest">Published articles and press releases</p>
 			<div className='min-h-screen grid grid-cols-3 gap-8 w-3/4 mx-auto pb-24'>
-				{imageArray.map((image, index) => (
-					<img key={index} src={image} alt={`featured press ${index}`} className='h-32 w-32 mx-auto' />
+				{imageArray.map((item, index) => (
+					<img key={index} src={item.image} alt={`featured press ${index}`}
+						name={item.link}
+						onClick={(e) => handleMouseOver(e)} className='h-32 w-32 mx-auto cursor-pointer hover:border rounded-full ease-in-out duration-100 transition-all' />
 				))}
 			</div>
 		</div>
