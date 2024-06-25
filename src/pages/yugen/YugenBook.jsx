c35e9960c58f0fb608372a49553d5b64b672c0c6
Mike Shields <codingshields@gmail.com>
2024-06-24 22:42:43 -0700

    yugen pages uploaded but quality is poor

--- src/pages/yugen/YugenBook.jsx
+++ src/pages/yugen/YugenBook.jsx
@@ -1,13 +1,49 @@
 import HTMLFlipBook from "react-pageflip";
-import cover from "../../assets/data/yugen/cover.jpg";
+import whiteLogo from "../../assets/images/logos/white-logo.png";
+import page1yugenvol2 from "../../assets/data/yugen/page1yugenvol2.jpg";
+import page2yugenvol2 from "../../assets/data/yugen/page2yugenvol2.jpg";
+import page3yugenvol2 from "../../assets/data/yugen/page3yugenvol2.jpg";
+import page4yugenvol2 from "../../assets/data/yugen/page4yugenvol2.jpg";
+import page5yugenvol2 from "../../assets/data/yugen/page5yugenvol2.jpg";
+import page6yugenvol2 from "../../assets/data/yugen/page6yugenvol2.jpg";
+import page7yugenvol2 from "../../assets/data/yugen/page7yugenvol2.jpg";
+import page8yugenvol2 from "../../assets/data/yugen/page8yugenvol2.jpg";
+import page9yugenvol2 from "../../assets/data/yugen/page9yugenvol2.jpg";
+import page10yugenvol2 from "../../assets/data/yugen/page10yugenvol2.jpg";
+import page11yugenvol2 from "../../assets/data/yugen/page11yugenvol2.jpg";
+import page12yugenvol2 from "../../assets/data/yugen/page12yugenvol2.jpg";
+import page13yugenvol2 from "../../assets/data/yugen/page13yugenvol2.jpg";
+import page14yugenvol2 from "../../assets/data/yugen/page14yugenvol2.jpg";
+import page15yugenvol2 from "../../assets/data/yugen/page15yugenvol2.jpg";
+import page16yugenvol2 from "../../assets/data/yugen/page16yugenvol2.jpg";
+import page17yugenvol2 from "../../assets/data/yugen/page17yugenvol2.jpg";
+import page18yugenvol2 from "../../assets/data/yugen/page18yugenvol2.jpg";
+import page19yugenvol2 from "../../assets/data/yugen/page19yugenvol2.jpg";
+import page20yugenvol2 from "../../assets/data/yugen/page20yugenvol2.jpg";
 const YugenBook = () => {
 	return (
-        <HTMLFlipBook width={300} height={500}>
-			<img src={cover} className='w-full h-full' />
-			<img src={cover} className='w-full h-full' />
-			<img src={cover} className='w-full h-full' />
-			<img src={cover} className='w-full h-full' />
-			<img src={cover} className='w-full h-full' />
+		<HTMLFlipBook width={300} height={600}>
+            <img src={whiteLogo} />
+			<img src={page1yugenvol2} />
+			<img src={page2yugenvol2} />
+			<img src={page3yugenvol2} />
+			<img src={page4yugenvol2} />
+			<img src={page5yugenvol2} />
+			<img src={page6yugenvol2} />
+			<img src={page7yugenvol2} />
+			<img src={page8yugenvol2} />
+			<img src={page9yugenvol2} />
+			<img src={page10yugenvol2} />
+			<img src={page11yugenvol2} />
+			<img src={page12yugenvol2} />
+			<img src={page13yugenvol2} />
+			<img src={page14yugenvol2} />
+			<img src={page15yugenvol2} />
+			<img src={page16yugenvol2} />
+			<img src={page17yugenvol2} />
+			<img src={page18yugenvol2} />
+			<img src={page19yugenvol2} />
+			<img src={page20yugenvol2} />
 		</HTMLFlipBook>
 	);
 };
